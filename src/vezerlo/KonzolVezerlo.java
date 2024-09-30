4e8961d4fd88854d75dd4cd47381dc64036d327f
DDorka02 <dobszaydorka@gmail.com>
2024-09-30 11:49:58 +0200

    kozos os commit

--- src/vezerlo/KonzolVezerlo.java
+++ src/vezerlo/KonzolVezerlo.java
@@ -1,25 +1,22 @@
 package vezerlo;
 
 import modell.JatekModell;
-import nezet.KonzolNezet;
-
+import nezet.CuiNezet;
 
 public class KonzolVezerlo {
     private JatekModell modell;
-    private KonzolNezet nezet;
+    private CuiNezet nezet;
 
-    public KonzolVezerlo(JatekModell modell, KonzolNezet nezet) {
+    public KonzolVezerlo(JatekModell modell, CuiNezet nezet) {
         this.modell = modell;
         this.nezet = nezet;
         
         start();
         
     }
-
     private void start() {
         nezet.megjelenit(modell.kezdes());
         modell.setValasztas(nezet.bekeres());
         nezet.megjelenit(modell.ellenorzes());
-        nezet.bezar();
     }
 }
