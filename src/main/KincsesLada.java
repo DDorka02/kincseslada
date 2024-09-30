0590e328c0762aec41e7fe775e75db32dfe4e1c5
DDorka02 <dobszaydorka@gmail.com>
2024-09-30 11:36:43 +0200

    uj felugro nezet

--- src/main/KincsesLada.java
+++ src/main/KincsesLada.java
@@ -3,8 +3,10 @@
 package main;
 
 import modell.JatekModell;
+import nezet.FelugroNezet;
 import nezet.KonzolNezet;
 import vezerlo.KonzolVezerlo;
+import vezerlo.FelugoVezerlo;
 
 
 public class KincsesLada {
@@ -12,8 +14,11 @@ public class KincsesLada {
 
     public static void main(String[] args) {
         JatekModell modell = new JatekModell();
-        KonzolNezet nezet = new KonzolNezet();
-        new KonzolVezerlo(modell, nezet);
+//        KonzolNezet nezet = new KonzolNezet();
+//        new KonzolVezerlo(modell, nezet);
+        
+        FelugroNezet nezet1 = new FelugroNezet();
+        new FelugoVezerlo(modell, nezet1);
     }
     
 }
